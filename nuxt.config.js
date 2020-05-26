0ede7d40095fd87fe27635f77ebcbb72a2fface1
kazutoshi <kazutoshi.yasutake@kikagaku.co.jp>
2020-05-26 12:56:15 +0900

    base_urlを更新

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 
 // const base_url = 'http://localhost:3000/'
-const base_url = 'https://kikagaku.co.jp/'
+const base_url = 'https://yasutake.kikagaku.net/'
 
 export default {
   mode: 'universal',
